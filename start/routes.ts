081644f765a15d7e4023ef444c30bfba1ed81988
enzo-mir <miraglioenzo93@gmail.com>
2023-10-06 16:16:55 +0200

    Design avec Tailwind css

--- start/routes.ts
+++ start/routes.ts
@@ -32,25 +32,32 @@ Route.post("/postStatus", async (ctx) => {
 Route.post("/addTodo", async (ctx) => {
   let body: Record<string, string> = ctx.request.body();
 
-  let insertionTodo = await Database.table("todo_blanks").insert({
-    title: body.title,
-    description: body.desc,
-    status: 0,
-  });
+  let getSameTitle = await Database.from("todo_blanks")
+    .select("*")
+    .where("title", body.title);
 
-  if (insertionTodo) {
-    ctx.response.status(200);
+  if (!getSameTitle.length) {
+    let insertionTodo = await Database.table("todo_blanks").insert({
+      title: body.title,
+      description: body.desc,
+      status: 0,
+    });
+
+    if (insertionTodo) {
+      ctx.response.status(200);
+      ctx.response.json({ message: null });
+    } else {
+      ctx.response.status(501);
+    }
   } else {
-    ctx.response.status(501);
+    ctx.response.json({ message: "Le titre doit être différent" });
   }
 });
 Route.post("/deleteTodo", async (ctx) => {
-  let { id  } = ctx.request.body();
+  let { id } = ctx.request.body();
   let deletePost = await Database.from("todo_blanks")
     .delete("*")
     .where("id", id);
 
-    if(deletePost) ctx.response.status(200)
-
-    
+  if (deletePost) ctx.response.status(200);
 });
